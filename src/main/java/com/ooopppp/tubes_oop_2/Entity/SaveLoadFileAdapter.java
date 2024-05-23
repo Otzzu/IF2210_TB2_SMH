5a2e268114e162271ca27e704b200872210b9a7d
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-05-23 17:51:46 +0700

    fix: fix sell dialog bug

--- src/main/java/com/ooopppp/tubes_oop_2/Entity/SaveLoadFileAdapter.java
+++ src/main/java/com/ooopppp/tubes_oop_2/Entity/SaveLoadFileAdapter.java
@@ -1,35 +1,35 @@
-package com.ooopppp.tubes_oop_2.Entity;
-
-public class SaveLoadFileAdapter  implements SaveLoadFile {
-
-    private ExternalSaveLoadFile externalSaveLoadFile;
-
-    public SaveLoadFileAdapter(ExternalSaveLoadFile externalSaveLoadFile) {
-        this.externalSaveLoadFile = externalSaveLoadFile;
-    }
-
-    @Override
-    public void loadData() {
-
-    }
-
-    @Override
-    public void loadPlayer() {
-
-    }
-
-    @Override
-    public void loadGameState() {
-
-    }
-
-    @Override
-    public void savePlayer() {
-
-    }
-
-    @Override
-    public void saveGameState() {
-
-    }
-}
+//package com.ooopppp.tubes_oop_2.Entity;
+//
+//public class SaveLoadFileAdapter  implements SaveLoadFile {
+//
+//    private ExternalSaveLoadFile externalSaveLoadFile;
+//
+//    public SaveLoadFileAdapter(ExternalSaveLoadFile externalSaveLoadFile) {
+//        this.externalSaveLoadFile = externalSaveLoadFile;
+//    }
+//
+//    @Override
+//    public void loadData() {
+//
+//    }
+//
+//    @Override
+//    public void loadPlayer() {
+//
+//    }
+//
+//    @Override
+//    public void loadGameState() {
+//
+//    }
+//
+//    @Override
+//    public void savePlayer() {
+//
+//    }
+//
+//    @Override
+//    public void saveGameState() {
+//
+//    }
+//}
